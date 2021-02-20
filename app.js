8a94b580855a2a795bbeac0c92b3b23cf0ebd7f9
robbridges <robbridges@users.noreply.github.com>
2021-02-19 20:17:36 -0800

    add routes and set view engine. As well as static files

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const pug = require('pug');
+const data = require('./data/data.json');
 
 const app = express();
+app.set('view engine', 'pug');
+
+app.use('/static', express.static('public'));
+
+app.get('/', (req,res) => {
+  res.render('index', data);
+})
+app.get('/about', (req,res) => {
+  res.render('about', data);
+})
 
 app.listen(3000, () => {
   console.log('Express is running on port 3000');
