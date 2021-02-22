2057da02f120142acaaedac4ca3f8473c8c807e3
robbridges <robbridges@users.noreply.github.com>
2021-02-21 18:24:34 -0800

    cleaner project look up method, throws error on weird project id arguments

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
+const e = require('express');
 const express = require('express');
 const pug = require('pug');
 const data = require('./data/data.json');
 
+// setting up the app and view engine
 const app = express();
 app.set('view engine', 'pug');
 
 app.use('/static', express.static('public'));
 
 
+/*
+Below we have our route files 
+*/
 
+/*
+Below are the routes that the app is programmed to go to
+*/
 app.get('/', (req,res) => {
   res.render('index', data);
 });
@@ -17,28 +25,37 @@ app.get('/about', (req,res) => {
   
   res.render('about', data);
 });
-
+/*
+Unique route that shows the project based on the id that is given. 
+*/
 app.get('/projects/:id', (req,res) => {
   const id = req.params.id;
-  const projectId = parseInt(id);
-  const project = data.projects.find(project => projectId === parseInt(project.id));
-  res.render('project', project); 
+  if (id > data.projects.length) {
+    const error = new Error('The project you are looking for does not exist');
+    err.status = 404;
+    next(err);
+  }
+  res.render('project', data.projects[id]); 
+  
 
 });
 
-app.use((req, res,) => {
+/*
+middle ware to handle 404 errors
+*/
+app.use((req, res) => {
   const err = new Error('The requested page was not found');
   err.status = 404;
   console.log(`There was an ${err.status} error`);
   console.log(err.stack);
   res.render('page-not-found', {error: err});
 });
-
-app.use((err, req, res) => {
+/*
+Middleware to handle any error that is not a 404 error. 
+*/
+app.use((err, req, res, next) => {
   res.locals.error = err;
   res.status(err.status);
-  console.log(`There was an ${err.status} error`);
-  console.log(err.stack);
   res.render('error', {error: err} );
 });
 
@@ -47,7 +64,7 @@ app.use((err, req, res) => {
 
 
 
-
+// we set up and run the app below.
 
 app.listen(3000, () => {
   console.log('Express is running on port 3000');
