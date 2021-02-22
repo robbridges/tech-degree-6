b7d3b41bbba6c19905619e1c5bc32060dd377614
robbridges <robbridges@users.noreply.github.com>
2021-02-21 17:48:11 -0800

    created error catching

--- app.js
+++ app.js
@@ -7,12 +7,16 @@ app.set('view engine', 'pug');
 
 app.use('/static', express.static('public'));
 
+
+
 app.get('/', (req,res) => {
   res.render('index', data);
-})
+});
+
 app.get('/about', (req,res) => {
+  
   res.render('about', data);
-})
+});
 
 app.get('/projects/:id', (req,res) => {
   const id = req.params.id;
@@ -20,7 +24,28 @@ app.get('/projects/:id', (req,res) => {
   const project = data.projects.find(project => projectId === parseInt(project.id));
   res.render('project', project); 
 
-})
+});
+
+app.use((req, res,) => {
+  const err = new Error('The requested page was not found');
+  err.status = 404;
+  console.log(`There was an ${err.status} error`);
+  console.log(err.stack);
+  res.render('page-not-found', {error: err});
+});
+
+app.use((err, req, res) => {
+  res.locals.error = err;
+  res.status(err.status);
+  console.log(`There was an ${err.status} error`);
+  console.log(err.stack);
+  res.render('error', {error: err} );
+});
+
+
+
+
+
 
 
 
