c0a7efed5bf9ffc7f6d1b703047ebbd29616113a
robbridges <robbridges@users.noreply.github.com>
2021-02-22 20:09:10 -0800

    cleaned up the error handling of /projects 404 errors

---
+++ README.md
@@ -0,0 +1,1 @@
+Changed the background of the portfolio to a dark blue #0346ac hexcode.
--- app.js
+++ app.js
@@ -26,16 +26,20 @@ app.get('/about', (req,res) => {
   res.render('about', data);
 });
 /*
-Unique route that shows the project based on the id that is given. 
+Unique route that shows the project based on the id that is given. It renders the 404 page if the project is less than 0 (Out of array index) or greater than the length of the array minus one
+as the project id is zero based index. It also renders the 404 page not found if someone tries to pass in a value that is not a number
 */
 app.get('/projects/:id', (req,res) => {
   const id = req.params.id;
-  if (id > data.projects.length) {
-    const error = new Error('The project you are looking for does not exist');
+  if (id > data.projects.length -1 || id < 0 || isNaN(id)) {
+    const err = new Error('The project you are looking for does not exist');
     err.status = 404;
-    next(err);
-  }
+    console.log(`There was an ${err.status} error`);
+    console.log(err.stack);
+    res.render('page-not-found', {error: err}); 
+  } else {
   res.render('project', data.projects[id]); 
+  }
   
 
 });
@@ -43,7 +47,7 @@ app.get('/projects/:id', (req,res) => {
 /*
 middle ware to handle 404 errors
 */
-app.use((req, res) => {
+app.use((req, res, next) => {
   const err = new Error('The requested page was not found');
   err.status = 404;
   console.log(`There was an ${err.status} error`);
@@ -56,6 +60,8 @@ Middleware to handle any error that is not a 404 error.
 app.use((err, req, res, next) => {
   res.locals.error = err;
   res.status(err.status);
+  console.log(`There was an ${err.status} error`);
+  console.log(err.stack);
   res.render('error', {error: err} );
 });
 
