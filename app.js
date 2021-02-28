54a13fd91de48464e8415af02d346b5b8f92ff02
robbridges <robbridges@users.noreply.github.com>
2021-02-27 21:14:55 -0800

    fixed error handling, got rid of invalid header response

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const e = require('express');
+
 const express = require('express');
 const pug = require('pug');
 const data = require('./data/data.json');
@@ -29,19 +29,16 @@ app.get('/about', (req,res) => {
 Unique route that shows the project based on the id that is given. It renders the 404 page if the project is less than 0 (Out of array index) or greater than the length of the array minus one
 as the project id is zero based index. It also renders the 404 page not found if someone tries to pass in a value that is not a number
 */
-app.get('/projects/:id', (req,res) => {
+
+
+app.get('/projects/:id', (req,res,next) => {
   const id = req.params.id;
   if (id > data.projects.length -1 || id < 0 || isNaN(id)) {
-    const err = new Error('The project you are looking for does not exist');
-    err.status = 404;
-    console.log(`There was an ${err.status} error`);
-    console.log(err.stack);
-    res.render('page-not-found', {error: err}); 
-  } 
-  res.render('project', data.projects[id]); 
-  
-  
-
+    next();
+    
+  } else { 
+  res.render('project', data.projects[id]);
+  }
 });
 
 /*
@@ -50,9 +47,7 @@ middle ware to handle 404 errors
 app.use((req, res, next) => {
   const err = new Error('The requested page was not found');
   err.status = 404;
-  console.log(`There was an ${err.status} error`);
-  console.log(err.stack);
-  res.render('page-not-found', {error: err});
+  next(err);
 });
 /*
 Middleware to handle any error that is not a 404 error. 
@@ -62,7 +57,11 @@ app.use((err, req, res, next) => {
   res.status(err.status);
   console.log(`There was an ${err.status} error`);
   console.log(err.stack);
-  res.render('error', {error: err} );
+  if (err.status = 404) {
+    res.render('page-not-found', {error: err});
+  } else {
+    res.render('error', {error: err} );
+  }
 });
 
 
